d2b1354ecc703734f22c990396e76dfe13f945ab
Alireza Prf <33633327+alirezaprf@users.noreply.github.com>
2020-05-01 20:16:08 +0430

    Update MainFrame.java

--- MainFrame.java
+++ MainFrame.java
@@ -1,11 +1,14 @@
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.SoftBevelBorder;
+
 import java.awt.*;
 import java.awt.color.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame {
-
+    
     private JPanel west, center, east;
     private final int Xsize = 600;
     private final int Ysize = 700;
@@ -20,20 +23,29 @@ public class MainFrame extends JFrame {
         west = new JPanel();
         center = new JPanel();
         east = new JPanel();
-        JSplitPane jPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, west, center);
+        JSplitPane jPane1 = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, west, center);
+        
         west.setBackground(Color.red);
         center.setBackground(Color.GREEN);
         east.setBackground(Color.blue);
 
         setLayout(layout);
 
-        JSplitPane jPane2 = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, jPane, east);
+        JSplitPane jPane2 = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, jPane1, east);
 
         add(jPane2);
         
 
-
+        InitProperties();
         
         setVisible(true);
     }
+
+    public void InitProperties()
+    {
+        
+    }
+
+
+
 }
