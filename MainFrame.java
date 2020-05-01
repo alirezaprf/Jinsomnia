1a478190018453122b1a89b57f90b2cb0174cdd5
Alireza Prf <33633327+alirezaprf@users.noreply.github.com>
2020-05-01 19:49:58 +0430

    bug fix

--- MainFrame.java
+++ MainFrame.java
@@ -1,41 +1,36 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.color.*;
-public class MainFrame extends JFrame
-{
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-    private JPanel west,center,east;
-    private final int Xsize=600;
-    private final int Ysize=700;
-    public MainFrame()
-    {
+public class MainFrame extends JFrame {
+
+    private JPanel west, center, east;
+    private final int Xsize = 600;
+    private final int Ysize = 700;
+
+    public MainFrame() {
         super("Jinsomnia");
-        setSize(Xsize,Ysize);
+        setSize(Xsize, Ysize);
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        GridLayout layout=new GridLayout(1,2);
+        GridLayout layout = new GridLayout(1, 2);
         setDefaultLookAndFeelDecorated(true);
-        
-        west=new JPanel();
-        center=new JPanel();
-        east=new JPanel();
-        JSplitPane jPane=new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, west, center);
+
+        west = new JPanel();
+        center = new JPanel();
+        east = new JPanel();
+        JSplitPane jPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, west, center);
         west.setBackground(Color.red);
         center.setBackground(Color.GREEN);
         east.setBackground(Color.blue);
-        
-        
-        // west
-        // center
-        // east
-        
+
         setLayout(layout);
 
-        // add(west);
-        // add(center);
-        JSplitPane jPane2=new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,jPane,east);
-        // add(jPane);
-        // add(east);
+        JSplitPane jPane2 = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, jPane, east);
+
         add(jPane2);
+        
 
 
         
