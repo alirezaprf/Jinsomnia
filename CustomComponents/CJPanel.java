2474614ff1316d73a355dd6bf945c21a5cf89f63
Alireza Prf <33633327+alirezaprf@users.noreply.github.com>
2020-05-10 03:15:18 +0430

    Revert "arraylist added"
    This reverts commit 0fa8e382fa4f2f29b0fe188a12652c4faf5cd5bd.

--- CustomComponents/CJPanel.java
+++ CustomComponents/CJPanel.java
@@ -2,7 +2,6 @@ package CustomComponents;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
 
 import Configs.AppTheme;
 
@@ -13,14 +12,9 @@ public class CJPanel extends JPanel {
      */
     private static final long serialVersionUID = -670276154319342504L;
     private int last = 1;
-    public ArrayList<JKeyValue> keyValues;
 
-    public CJPanel(boolean isChangable) {
-
+    public CJPanel() {
         super();
-        
-           
-        keyValues=new ArrayList<JKeyValue>();
         this.setBackground(AppTheme.Background);
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         
@@ -30,13 +24,10 @@ public class CJPanel extends JPanel {
         add(new JKeyValue(false, l -> {
             JKeyValue jk = new JKeyValue(true,null);
             add(jk, last++);
-            keyValues.add(jk);
             revalidate();
             jk.keyFiled.requestFocus();
             jk.addDeleter(deleter->{
-                keyValues.remove(jk);
                 remove(jk);
-                
                 last--;
                 revalidate();
             });
@@ -50,21 +41,9 @@ public class CJPanel extends JPanel {
         this.setBackground(color);
     }
 
-    
-    public void AddKeyValue(String Key,String Value)
-    {
-        JKeyValue keyValue=new JKeyValue(false, null);
-        keyValue.keyFiled.setText(Key);
-        keyValue.valueFiled.setText(Value);
-        keyValues.add(keyValue);
-    }
-    public String getKeyAt(int index)
-    {
-        return keyValues.get(index).keyFiled.getText();
-    }
-    public String getValueAt(int index)
-    {
-        return keyValues.get(index).valueFiled.getText();
+    public CJPanel(boolean Opaque) {
+        super();
+        this.setOpaque(Opaque);
     }
 
 }
