c7d01c3e3fe495fc5786d60bd9ba7e68f88b8384
Alireza Prf <33633327+alirezaprf@users.noreply.github.com>
2020-05-09 14:40:36 +0430

    Update JKeyValue.java

--- CustomComponents/JKeyValue.java
+++ CustomComponents/JKeyValue.java
@@ -7,70 +7,80 @@ import Configs.AppTheme;
 import java.awt.*;
 
 public class JKeyValue extends JPanel {
-    
-    private boolean isEnabled=true;
+
+    private boolean isEnabled = true;
     public JTextFiledCustom keyFiled;
     public JTextFiledCustom valueFiled;
     public JButton delete;
     public JCheckBox active;
-    public JKeyValue(boolean changeable)
-    {
+
+    /**
+     * 
+     * @param changeable True for input
+     * False for Just output
+     * @param parentGbc The Parent GBC for adding component directly 
+     */
+    public JKeyValue(boolean changeable) {
         super();
-        isEnabled=changeable;
+        isEnabled = changeable;
         setLayout(new GridBagLayout());
-        GridBagConstraints gbc=new GridBagConstraints();
-        gbc.anchor=GridBagConstraints.WEST;
-        gbc.fill=GridBagConstraints.BOTH;
-        gbc.insets=new Insets(5,10,5,10);
-        gbc.weightx=50;
-        gbc.weighty=1;
-        gbc.gridx=0;
-        gbc.gridy=0;
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.insets = new Insets(5, 10, 5, 10);
+        gbc.weightx = 50;
+        gbc.weighty = 1;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
         setBackground(AppTheme.Background);
-        keyFiled=new JTextFiledCustom("Key");
-        valueFiled=new JTextFiledCustom("Value");
+        keyFiled = new JTextFiledCustom("Key");
+        valueFiled = new JTextFiledCustom("Value");
 
         keyFiled.setBackground(AppTheme.Background);
         valueFiled.setBackground(AppTheme.Background);
 
         keyFiled.setForeground(AppTheme.text);
         valueFiled.setForeground(AppTheme.text);
-        
+
         keyFiled.setBorder(BorderFactory.createLoweredSoftBevelBorder());
         valueFiled.setBorder(BorderFactory.createLoweredSoftBevelBorder());
-        
-        
-        ImageIcon trashIcon=new ImageIcon("assets/trash.png");
-        Image scaled=trashIcon.getImage().getScaledInstance(25, 25,  java.awt.Image.SCALE_SMOOTH); trashIcon.getImage().getScaledInstance(120, 120,  java.awt.Image.SCALE_SMOOTH); 
+
+        ImageIcon trashIcon = new ImageIcon("assets/trash.png");
+        Image scaled = trashIcon.getImage().getScaledInstance(25, 25, java.awt.Image.SCALE_SMOOTH);
+        trashIcon.getImage().getScaledInstance(120, 120, java.awt.Image.SCALE_SMOOTH);
         trashIcon.setImage(scaled);
 
-        add(keyFiled,gbc);
+        add(keyFiled, gbc);
         gbc.gridx++;
-        add(valueFiled,gbc);
-
-        gbc.gridx++;
-        gbc.weightx=1;
-        active=new JCheckBox();
-        active.setSelected(true);
-        add(active,gbc);
-        active.setBackground(active.getParent().getBackground());
-
+        add(valueFiled, gbc);
         
+        /**
+         * true for input
+         * false for just output
+         */
+        if (changeable) {
+            gbc.gridx++;
+            gbc.weightx = 1;
+            active = new JCheckBox();
+            active.setSelected(true);
+            add(active, gbc);
+            active.setBackground(active.getParent().getBackground());
+
+            delete = new JButton(trashIcon);
+            delete.setBorder(BorderFactory.createEmptyBorder());
+            delete.setFocusPainted(true);
+
+            gbc.gridx++;
+            add(delete, gbc);
+            delete.setBackground(delete.getParent().getBackground());
         
-        delete=new JButton(trashIcon);
-        delete.setBorder(BorderFactory.createEmptyBorder());
-        delete.setFocusPainted(true);
-
-
-
-        gbc.gridx++;
-        add(delete,gbc);       
-        delete.setBackground(delete.getParent().getBackground());
-
+            delete.addActionListener(l -> {
+                
+            });
         
+        }
+
 
     }
-
-
 
 }
