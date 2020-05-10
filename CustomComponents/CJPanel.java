09ba5cc67b72ca5536ebc46836143a1da569cc67
Alireza Prf <33633327+alirezaprf@users.noreply.github.com>
2020-05-10 20:19:57 +0430

    clear all added

--- CustomComponents/CJPanel.java
+++ CustomComponents/CJPanel.java
@@ -14,15 +14,17 @@ public class CJPanel extends JPanel {
     private int last = 1;
     public ArrayList<JKeyValue> KeyValueDatas;
     private boolean chagable=false;
-    public CJPanel() {
+    public CJPanel(boolean IsChangeAllowed) {
+        
         super();
+        chagable=IsChangeAllowed;
         KeyValueDatas=new ArrayList<JKeyValue>();
         this.setBackground(AppTheme.Background);
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         
         add(Box.createVerticalStrut(20));
         
-        
+        if(chagable)
         add(new JKeyValue(false, l -> {
             JKeyValue jk = new JKeyValue(true,null);
             add(jk, last++);
@@ -51,9 +53,10 @@ public class CJPanel extends JPanel {
         keyValue.keyFiled.setText(Key);
         keyValue.valueFiled.setText(Value);
         KeyValueDatas.add(keyValue);
-        add(keyValue,last);
+        add(keyValue,last++);
+        add(Box.createVerticalStrut(10),last++);
         revalidate();
-        
+            
     }
     @Override
     public String toString() {
@@ -61,7 +64,13 @@ public class CJPanel extends JPanel {
         return super.toString()+"\n=>"+chagable
         +"\n=>"+KeyValueDatas.toString();
     }
-
+    public void clear()
+    {
+        removeAll();
+        KeyValueDatas.clear();
+        updateUI();
+    }
+    
     
 
 }
