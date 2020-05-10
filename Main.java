f16397098d225b03c2d5f866ff81719e2c6789f0
Alireza Prf <33633327+alirezaprf@users.noreply.github.com>
2020-05-10 20:19:17 +0430

    change defualt font

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
+import CustomComponents.ComponentModifier;
+import java.awt.Font;
 
+import javax.swing.plaf.FontUIResource;
 public class Main {
     public static void main(String[] args) {
+        Font defualtFont=new Font("Comic Sans MS",Font.BOLD,14);
+        FontUIResource fResource=new FontUIResource(defualtFont);
+        ComponentModifier.setUIFont(fResource);
         new MainFrame();
         
     }
