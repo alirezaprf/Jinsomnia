88101838dc29f54cd12d14313bf38181a308c338
Alireza Prf <33633327+alirezaprf@users.noreply.github.com>
2020-05-13 02:22:21 +0430

    Update Cli.java

--- Cli/Cli.java
+++ Cli/Cli.java
@@ -1,13 +1,13 @@
 import java.util.Arrays;
 
-import org.apache.commons.*;
+
 import org.apache.commons.cli.*;
 
 import Models.reqType;
 
 public class Cli {
    
-
+   //#region variables
    private final Option methodChange = new Option("M", "method", true, "change method [get,post,put,patch,delete] ");
    private final Option headers = new Option("H", "Headers", true, "input headers as String");
    private final Option response_headers = new Option("i", false, "shows response's headers");
@@ -22,6 +22,7 @@ public class Cli {
    private final Option upload = new Option(null, "upload", true,
           "gets binary file to send - *third priority over other message bodies");       
    private final Options options = new Options();
+   private CommandLine cmd=null;
    public String[] args;
 
    private String URL="";
@@ -29,6 +30,10 @@ public class Cli {
    private boolean follow=false;
    private String headerString="";
    private Object body;
+
+   //#endregion
+
+
 
    /**
     * 
@@ -38,19 +43,20 @@ public class Cli {
       this.args=Args;
       Initilize();
       CommandLineParser parser = new DefaultParser();
-      CommandLine cmd=null;
+      
       try {
          cmd = parser.parse(options, args);
       } catch (ParseException e) {
          e.printStackTrace();
          System.exit(-1);
       }
+
+      
       
       if(cmd.hasOption(help.getLongOpt()) || cmd.hasOption(help.getOpt()))
       {
          showHelp();
       }
-      if(cmd.hasOption(methodChange.getOpt()) || cmd.hasOption(methodChange.getOpt()))
       
 
    }
@@ -72,6 +78,31 @@ public class Cli {
    }
    //#endregion Initilize
 
+
+   /**
+    * 
+    * @param option targert argument or option
+    * @return whther it's in the argument or not
+    */
+   public boolean hasOption(Option option)
+   {
+      if(cmd == null)
+      return false;
+
+      if(option.getOpt()==null)
+      return cmd.hasOption(option.getLongOpt());
+
+      if(option.getLongOpt()==null)
+      return cmd.hasOption(option.getOpt());
+
+
+
+
+      return (cmd.hasOption(option.getOpt()) || 
+              cmd.hasOption(option.getLongOpt()));
+
+
+   }
 
    /**
     * Showing the help of program
